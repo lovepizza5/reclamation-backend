fdb9a0505ad36a463e6338960a1e6f85814b0d2a
melek <lovepizzahf@gmail.com>
2026-01-15 01:27:47 +0100

    Use DATABASE_URL on Railway, fallback to SQLite locally

--- reclaim_dashboard/settings.py
+++ reclaim_dashboard/settings.py
@@ -45,13 +45,23 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = "reclaim_dashboard.wsgi.application"
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=not DEBUG
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = []
